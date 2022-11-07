a4f0915514aa87b446374a304b85ec7b2970fee7
vaik-info <info@vaikobo.com>
2022-11-07 13:55:23 +0900

    first commit

--- callbacks/save_callback.py
+++ callbacks/save_callback.py
@@ -13,11 +13,8 @@ class SaveCallback(tf.keras.callbacks.Callback):
         self.prefix = prefix
 
     def on_epoch_end(self, epoch, logs=None):
-        # create save model dir
         loss_string = "_".join([f'{k}_{v:.4f}' for k, v in logs.items()])
         save_model_name = f'{self.prefix}_epoch-{epoch}_{loss_string}'
         output_model_dir_path = os.path.join(self.save_model_dir_path, save_model_name)
         os.makedirs(output_model_dir_path, exist_ok=True)
-
-        # save model
         self.model.save(os.path.join(f'{output_model_dir_path}', 'model'))
