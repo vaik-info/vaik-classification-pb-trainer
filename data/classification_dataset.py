9b59456da4aa208dd01390b986250ba252d086a2
vaik-info <info@vaikobo.com>
2022-11-07 10:26:37 +0900

    first commit

---
+++ data/classification_dataset.py
@@ -0,0 +1,93 @@
+import tensorflow as tf
+import glob
+import os
+import re
+import random
+from tqdm import tqdm
+import numpy as np
+
+from data import ops
+
+
+class ClassificationDataset:
+    input_size = None
+    classes = None
+    image_dict = None
+    aug_ratio = 0.5
+
+    def __new__(cls, input_dir_path, classes, input_size=(256, 256)):
+        cls.classes = classes
+        cls.input_size = input_size
+        cls.image_dict = cls._prepare_image_dict(input_dir_path, classes)
+        cls.output_signature = (tf.TensorSpec(name=f'image', shape=(input_size[0], input_size[1], 3), dtype=tf.uint8),
+                                tf.TensorSpec(name=f'class_index', shape=(), dtype=tf.int32))
+        dataset = tf.data.Dataset.from_generator(
+            cls._generator,
+            output_signature=cls.output_signature
+        )
+        return dataset
+
+    @classmethod
+    def _generator(cls):
+        while True:
+            class_index = random.choice(list(range(len(cls.classes))))
+            class_label = cls.classes[class_index]
+            image_path = random.choice(cls.image_dict[class_label])
+            tf_image = tf.image.decode_image(tf.io.read_file(image_path), channels=3)
+            if random.uniform(0.0, 1.0) < cls.aug_ratio:
+                tf_image = cls._data_aug(tf_image.numpy())
+            tf_image = tf.image.resize(tf_image, (cls.input_size[0], cls.input_size[1]), preserve_aspect_ratio=True)
+
+            # insert
+            canvas_image = tf.image.pad_to_bounding_box(tf_image, 0, 0, cls.input_size[0], cls.input_size[1])
+            yield (
+                tf.convert_to_tensor(canvas_image),
+                tf.convert_to_tensor(class_index)
+            )
+
+    @classmethod
+    def _prepare_image_dict(cls, input_dir_path, classes):
+        image_dict = {}
+        for class_label in classes:
+            dir_path_list = glob.glob(os.path.join(input_dir_path, f'**/{class_label}/'), recursive=True)
+            if len(dir_path_list) > 0:
+                dir_path = dir_path_list[0]
+            else:
+                print(f'not found:{class_label}')
+            image_dict[class_label] = []
+            image_path_list = [file_path for file_path in glob.glob(os.path.join(dir_path, '**/*.*'), recursive=True) if
+                               re.search('.*\.(png|jpg|bmp)$', file_path)]
+            for image_path in tqdm(image_path_list):
+                image_dict[class_label].append(image_path)
+        return image_dict
+
+    @classmethod
+    def _data_aug(cls, np_image: np.array, random_r_ratio=0.25):
+        np_image = ops.random_scale(np_image)
+        np_image = ops.random_resize(np_image)
+        np_image = ops.random_flip(np_image)
+        np_image = ops.random_padding(np_image)
+        np_image = ops.random_hsv(np_image, random_ratio=random_r_ratio)
+        return np_image
+
+
+class TestClassificationDataset(ClassificationDataset):
+    max_test_sample = None
+
+    def __new__(cls, input_dir_path, classes, max_test_sample, input_size=(256, 256)):
+        cls.max_test_sample = max_test_sample
+        return super(TestClassificationDataset, cls).__new__(cls, input_dir_path, classes, input_size)
+
+    @classmethod
+    def _generator(cls):
+        for class_index, class_label in enumerate(cls.classes):
+            for image_index, image_path in enumerate(cls.image_dict[class_label]):
+                if image_index > cls.max_test_sample:
+                    break
+                tf_image = tf.image.decode_image(tf.io.read_file(image_path), channels=3)
+                tf_image = tf.image.resize(tf_image, (cls.input_size[0], cls.input_size[1]), preserve_aspect_ratio=True)
+                canvas_image = tf.image.pad_to_bounding_box(tf_image, 0, 0, cls.input_size[0], cls.input_size[1])
+                yield (
+                    tf.convert_to_tensor(canvas_image),
+                    tf.convert_to_tensor(class_index)
+                )
---
+++ dump_dataset.py
@@ -0,0 +1,45 @@
+import os
+import argparse
+from tqdm import tqdm
+from PIL import Image
+import tensorflow as tf
+
+from data import classification_dataset
+
+
+def main(input_image_dir_path, classes_txt_path, sample_num, image_height, image_width, output_dir_path):
+    os.makedirs(output_dir_path, exist_ok=True)
+    classes = []
+    with open(classes_txt_path) as f:
+        for line in f:
+            classes.append(line.strip())
+
+    TrainDataset = type(f'TrainDataset', (classification_dataset.ClassificationDataset,), dict())
+    train_dataset = TrainDataset(input_image_dir_path, classes, (image_height, image_width))
+    train_dataset = train_dataset.padded_batch(batch_size=1, padding_values=(tf.constant(0, dtype=tf.uint8), tf.constant(0, dtype=tf.int32)))
+    train_dataset = iter(train_dataset)
+    for index in tqdm(range(sample_num)):
+        image, class_index = train_dataset.get_next()
+        class_index = class_index.numpy()[0]
+        image = image.numpy()[0]
+        output_image_path = os.path.join(output_dir_path, f'{classes[class_index]}_{index:09d}.jpg')
+        Image.fromarray(image).save(output_image_path, quality=100, subsampling=0)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='train')
+    parser.add_argument('--input_image_dir_path', type=str,
+                        default=os.path.expanduser('~/.vaik-mnist-classification-dataset/train'))
+    parser.add_argument('--classes_txt_path', type=str,
+                        default=os.path.expanduser('~/.vaik-mnist-classification-dataset/classes.txt'))
+    parser.add_argument('--sample_num', type=int, default=25000)
+    parser.add_argument('--image_height', type=int, default=256)
+    parser.add_argument('--image_width', type=int, default=256)
+    parser.add_argument('--output_dir_path', type=str,
+                        default=os.path.expanduser('~/.vaik-mnist-classification-dataset/dump'))
+    args = parser.parse_args()
+
+    args.input_image_dir_path = os.path.expanduser(args.input_image_dir_path)
+    args.classes_txt_path = os.path.expanduser(args.classes_txt_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    main(args.input_image_dir_path, args.classes_txt_path, args.sample_num, args.image_height, args.image_width, args.output_dir_path)
